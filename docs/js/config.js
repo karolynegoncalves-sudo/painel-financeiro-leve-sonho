220fd171e435839f7f4caa9337f43ac5eca1f3c5
karolynegoncalves-sudo <karolyne.goncalves@esags.edu.br>
2026-07-25 16:06:16 -0300

    Configura Client ID do Google e URL do Apps Script no site

--- docs/js/config.js
+++ docs/js/config.js
@@ -2,6 +2,6 @@
 // APPS_SCRIPT_URL = a URL "/exec" que aparece quando você implanta o Web App.
 // GOOGLE_CLIENT_ID = o OAuth Client ID criado no Google Cloud Console.
 window.PAINEL_CONFIG = {
-  APPS_SCRIPT_URL: 'COLE_AQUI_A_URL_DO_APPS_SCRIPT',
-  GOOGLE_CLIENT_ID: 'COLE_AQUI_O_GOOGLE_CLIENT_ID'
+  APPS_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbzNjUUKJahedkTd3Irgnwn8m_mpEq4Hdw4p6PLNWssgUI1nNourPnMeDiyhVBVNjiNl/exec',
+  GOOGLE_CLIENT_ID: '220743716320-ia319tnsqqj0mlr7hre4bi0m6qi658vm.apps.googleusercontent.com'
 };
